5a4367df338d22d70386ded88790ba9ab15b43c3
Ava Gonick <144552066+avagonick@users.noreply.github.com>
2024-04-20 16:39:33 -0700

    adding backend

---
+++ UserDetails.js
@@ -0,0 +1,14 @@
+//make schema for data
+const mongoose = require('mongoose');
+
+//so create a user schema, that is in the collection UserInfo
+const User = new mongoose.Schema({
+    name:String
+}, {
+    collection:"UserInfo"
+});
+
+//export this schema
+mongoose.model("UserInfo", User)
+
+//edit user data structure so that it also holds this the different values 
