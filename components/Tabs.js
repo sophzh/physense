4724a92effdb25b3f743ddcb9dd02c0e3303670d
sophzh <sophzh@ucla.edu>
2024-04-20 13:20:41 -0700

    working nav

--- components/Tabs.js
+++ components/Tabs.js
@@ -1,11 +1,12 @@
-import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import HomePage from '../pages/Home';
 
 const Tab = createBottomTabNavigator();
 
 export default function Tabs() {
-    <Tab.Navigator>
-        <Tab.Screen name='homepage' component={HomePage} />
-    </Tab.Navigator>
+    return(
+        <Tab.Navigator>
+            <Tab.Screen name='homepage' component={HomePage} />
+        </Tab.Navigator>
+    );
 }
