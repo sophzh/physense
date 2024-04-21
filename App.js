462b3b6424b689f06ad93afb48a37ea015ae6d67
sophzh <sophzh@ucla.edu>
2024-04-20 17:10:24 -0700

    Update App.js

--- App.js
+++ App.js
@@ -2,8 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import QuestionWithSlider from './components/SlideComponent';
+
+import Login from './pages/Login';
 import Tabs from './components/Tabs';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
